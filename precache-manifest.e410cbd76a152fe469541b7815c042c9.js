93d74c3926df0c51b1caa9ca1bce5da989eeca80
Liran <kalitin.alecsey.01@gmail.com>
2022-12-16 21:03:35 +0300

    Updates

---
+++ precache-manifest.e410cbd76a152fe469541b7815c042c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "77b971e2eb09549a0243",
+    "url": "/portfolio/static/css/main.50cf324f.chunk.css"
+  },
+  {
+    "revision": "77b971e2eb09549a0243",
+    "url": "/portfolio/static/js/main.77b971e2.chunk.js"
+  },
+  {
+    "revision": "a494808147a9c45a2c58",
+    "url": "/portfolio/static/js/1.a4948081.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "c9ad3050e033455d44cf22bcbb6b164d",
+    "url": "/portfolio/static/media/self.c9ad3050.png"
+  },
+  {
+    "revision": "3e7d6f3e3bc72202f0b77f962ab8e8ca",
+    "url": "/portfolio/static/media/spridakunskap.3e7d6f3e.png"
+  },
+  {
+    "revision": "1c1a35765f2129ac1d292ab4b47e29e5",
+    "url": "/portfolio/static/media/epolitiker.1c1a3576.png"
+  },
+  {
+    "revision": "35482516518aeae06e588f433c116dbe",
+    "url": "/portfolio/index.html"
+  }
+];
